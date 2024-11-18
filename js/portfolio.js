13e87527f2a2c8c7e6ef9d5de4aabd5e659c22d3
Marcos-Pablo-Dev <marcosdev.redes@gmail.com>
2024-11-18 13:41:11 -0300

    update

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,19 +1,24 @@
 const portfolioItem = document.querySelectorAll('.portfolio-items');
 
 portfolioItem.forEach( item => {
-    item.addEventListener('mouseenter', () => {
-        item.classList.add('active')
+    item.addEventListener('mousemove', () => {
+        item.classList.add('port-active')
     });
 
     item.addEventListener('mouseout', () => {
-        item.classList.remove('active')
+        item.classList.remove('port-active')
     });
 
     item.addEventListener('click', () => {
-        const currentActive = document.querySelector('.portfolio-items.active');
-        if (currentActive && currentActive !== item){
-            currentActive.classList.remove('active');
+        const currentActive = document.querySelector('.portfolio-items.port-active')
+
+        if (item.classList.contains('port-active')) {
+            item.classList.remove('port-active')
+        } else if (currentActive && currentActive !== item){
+            currentActive.classList.remove('port-active');
+            item.classList.add('port-active')
+        } else {
+            item.classList.add('port-active')
         }
-        item.classList.add('active')
     });
 });
