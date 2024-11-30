30743ad0ef59f7f9fbdd9e34618d93e76fb446af
Marcos-Pablo-Dev <marcosdev.redes@gmail.com>
2024-11-29 21:42:27 -0300

    update

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,16 +1,37 @@
 const noJs = document.querySelectorAll('.no-js')
 const portfolioItem = document.querySelectorAll('.portfolio-items');
+let fullImg = 0;
+let itemActive = '';
 
 window.addEventListener('load', classRemove());
 window.addEventListener('resize', classRemove());
 
 function classRemove() {
-    noJs.forEach(item => {
-        item.classList.remove('no-js')
+    noJs.forEach(i => {
+        i.classList.remove('no-js')
     })
 }
 
 portfolioItem.forEach( item => {
+    let idItem = item.getAttribute('id');
+
+    item.addEventListener('click', () => {
+        if(fullImg == 0){
+            document.querySelector('#'+ idItem).classList.add('port-active');
+            fullImg = 1;
+            itemActive = idItem;
+        } else if(fullImg == 1) {
+            if (idItem === itemActive) {
+                document.querySelector('#'+ idItem).classList.remove('port-active');
+                fullImg = 0;
+            } else {
+                document.querySelector('#'+ itemActive).classList.remove('port-active');
+                document.querySelector('#'+ idItem).classList.add('port-active');
+                itemActive = idItem;
+            }
+        }
+    })
+
     item.addEventListener('mousemove', () => {
         item.classList.add('port-active')
     });
@@ -18,17 +39,4 @@ portfolioItem.forEach( item => {
     item.addEventListener('mouseout', () => {
         item.classList.remove('port-active')
     });
-
-    item.addEventListener('click', () => {
-        const currentActive = document.querySelector('.portfolio-items.port-active')
-
-        if (item.classList.contains('port-active')) {
-            item.classList.remove('port-active')
-        } else if (currentActive && currentActive !== item){
-            currentActive.classList.remove('port-active');
-            item.classList.add('port-active')
-        } else {
-            item.classList.add('port-active')
-        }
-    });
-});
+})
